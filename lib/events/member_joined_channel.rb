feb4c97b8a8ed72cd4a2959b17262fe4a7992d69
Rutul Davè <rutuldave@gmail.com>
2022-08-03 10:45:46 -0400

    Cleanup code to make it work on a Linux web server

--- lib/events/member_joined_channel.rb
+++ lib/events/member_joined_channel.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+require 'os'
+
 SlackRubyBotServer::Events.configure do |config|
   config.on :event, ['event_callback', 'member_joined_channel'] do |event|
     team = Team.where(team_id: event[:event][:team]).first || raise("Cannot find team with ID #{event[:event][:team]}.")
@@ -10,7 +14,14 @@ SlackRubyBotServer::Events.configure do |config|
     channel = slack_client.conversations_info(channel: event[:event][:channel])
     channel_name = channel[:channel][:name]
 
-    slack_client.chat_postMessage(channel: event[:event][:channel], text: "Welcome #{user_name} to #{channel_name}!")
+    if OS.linux?
+      fork { exec 'mpg123', 'assets/welcome.mp3' }
+    elsif OS.mac?
+      fork { exec 'afplay', 'assets/welcome.mp3' }
+    else
+      event.logger.info 'Cannot play audio message.'
+    end
+    # slack_client.chat_postMessage(channel: event[:event][:channel], text: "Welcome #{user_name} to #{channel_name}!")
     event.logger.info "User #{user_name} joined #{channel_name}."
 
     { ok: true }
--- lib/events/message.rb
+++ lib/events/message.rb
@@ -1,12 +1,23 @@
+# frozen_string_literal: true
+
+require 'os'
+
 evil_quotes = {
-  "welcome" => "welcome.mp3",
-  "New release published" => "are-those-sharks-with-laser-beams-attached-to-their-heads.mp3",
-  "BREAKING NEWS" => "why_make_trillions.mp3",
+  'breaking news' => 'one_million_dollars.mp3',
+  'pull request merged' => 'begin_deploy.mp3',
+  'new release published' => 'begin_deploy.mp3',
+  'new issue created' => 'not_tollerate.mp3',
+  'lat-binding-issues' => 'look_what_you_did.mp3',
+  'gratitudes' => 'you_are_the_best_evil_son_an_evil_dad_could_ever_ask_for.mp3'
 }
 
 SlackRubyBotServer::Events.configure do |config|
   config.on :event, ['event_callback', 'message'] do |event|
-    team = Team.where(team_id: event[:event][:team]).first || raise("Cannot find team with ID #{event[:event][:team]}.")
+    team = Team.where(team_id: event[:event][:team]).first
+    if team.nil?
+      event.logger.error "Cannot find team with ID #{event[:event][:team]}."
+      break
+    end
 
     slack_client = Slack::Web::Client.new(token: Team.first.token)
 
@@ -14,13 +25,18 @@ SlackRubyBotServer::Events.configure do |config|
     channel_name = channel[:channel][:name]
     message = event[:event][:text]
 
-    if evil_quotes.keys.any? { |word| message.include?(word) }
-      key_for_quote = ""
-      evil_quotes.keys.each do |substring|
-        key_for_quote = substring if message.include? substring
+    if evil_quotes.keys.any? { |word| message.downcase.include?(word) }
+      key_for_quote = ''
+      evil_quotes.each_key do |substring|
+        key_for_quote = substring if message.downcase.include? substring
       end
-      ## works on mac
-      fork{ exec 'afplay', "../../assets/#{evil_quotes[key_for_quote]}" }
+      if OS.linux?
+        fork { exec 'mpg123', "assets/#{evil_quotes[key_for_quote]}" }
+      elsif OS.mac?
+        fork { exec 'afplay', "assets/#{evil_quotes[key_for_quote]}" }
+      else
+        event.logger.info "Cannot play audio message: #{event[:event][:text]} in #{channel_name}."
+      end
     end
 
     event.logger.info "Received message: #{event[:event][:text]} in #{channel_name}."
